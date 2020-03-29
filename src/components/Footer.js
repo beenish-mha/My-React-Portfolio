684289c298b9af7a9323539a6901441505672ce9
beenish <beeni.khurram@gmail.com>
2020-03-29 11:52:55 +0100

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style.css";
+function Footer() {
+  return (
+    <footer class="container-fluid text-center">
+      <p>copy rights@</p>
+    </footer>
+  );
+}
+
+export default Footer;
